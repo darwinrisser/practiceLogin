10cd9691eb7cff7d4b4c54b5dbabc037a91c0e7e
Darwin Risser <riser.darwin@gmail.com>
2019-06-01 22:51:35 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class User {
+  constructor(email, password){
+    this.email = email;
+    this.password = password;
+    this.id = 'random'
+  }
+}
+
+class App extends Component {
+  state={
+    emailInput: "",
+    passwordInput: ""
+  }
+
+  updateEmail = event => {
+    const newEmail = event.target.value;
+    this.setState({emailInput:newEmail})
+  }
+
+  updatePassword = event => {
+    const newPassword = event.target.value;
+    this.setState({passwordInput:newPassword})
+  }
+
+  createUser = (e) => {
+    e.preventDefault();
+    const email = this.state.emailInput;
+    const password = this.state.passwordInput;
+    const user = new User(email,password);
+    console.log(user);
+    this.setState({emailInput:''});
+    this.setState({passwordInput:''});
+  }
+
+  render(){
+    const formStyle = {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '200px'
+    }
+
+    return (
+      <div className="App">
+        <h1>Login</h1>
+        <form style={formStyle} onSubmit={this.createUser}>
+          E-mail: <input value={this.state.emailInput} onChange={this.updateEmail}/>
+          Password: <input value={this.state.passwordInput} type="password" onChange={this.updatePassword}/>
+          <input type="submit" value="Submit"/>
+        </form>
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
